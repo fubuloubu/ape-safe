a1ef8f86fc0f2a8c49235913c61916e43b486c31
fubuloubu <3859395+fubuloubu@users.noreply.github.com>
2024-03-11 22:31:52 -0400

    fix(API): client accepts empty signatures

--- ape_safe/client/__init__.py
+++ ape_safe/client/__init__.py
@@ -133,7 +133,7 @@ class SafeClient(BaseSafeClient):
                 b"",
             )
         )
-        post_dict: Dict = {"signature": signature.hex(), "origin": ORIGIN}
+        post_dict: Dict = {"signature": signature.hex() if signature else None, "origin": ORIGIN}
 
         for key, value in tx_data.model_dump(by_alias=True, mode="json").items():
             if isinstance(value, HexBytes):
@@ -212,6 +212,7 @@ class SafeClient(BaseSafeClient):
 
     def add_delegate(self, delegate: AddressType, label: str, delegator: AccountAPI):
         # TODO: Replace this by adding raw hash signing into supported account plugins
+        #       See: https://github.com/ApeWorX/ape/issues/1962
         if not isinstance(delegator, KeyfileAccount):
             raise ActionNotPerformedError("Need access to private key for this method.")
 
@@ -236,6 +237,7 @@ class SafeClient(BaseSafeClient):
 
     def remove_delegate(self, delegate: AddressType, delegator: AccountAPI):
         # TODO: Replace this by adding raw hash signing into supported account plugins
+        #       See: https://github.com/ApeWorX/ape/issues/1962
         if not isinstance(delegator, KeyfileAccount):
             raise ActionNotPerformedError("Need access to private key for this method.")
 
